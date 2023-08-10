0b5b9c2fef879d244c7cbebddb303a9affca35c3
eunseo <92281453+les0498@users.noreply.github.com>
2023-08-11 00:36:46 +0900

    react-tictactoe-app

--- react-tictactoe-app/src/components/Board.js
+++ react-tictactoe-app/src/components/Board.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 import "./Board.css";
 
-export default class Board extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      Squares: Array(9).fill(null)
-    }
+const Board = () => {
+  
+  const [squares,setSqures] = useState(Array(9).fill(null)); //getter와 setter
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    newSquares[i] = 'X';
+    setSqures(newSquares);
   }
 
-  renderSquare(i) {
-    return <Square value={this.state.Squares[i]} />
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} 
+        onClick={() => handleClick(i)} />
   }
 
-  render() {
     return (
       <div>
         <div className='status'>Next Player: X, O</div>
         <div className='board-row'>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className='board-row'>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className='board-row'>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
     )
-  }
 }
+export default Board;
+
--- react-tictactoe-app/src/components/Square.js
+++ react-tictactoe-app/src/components/Square.js
@@ -1,16 +1,17 @@
 import React from "react"; 
 import "./Square.css";
 
-export default class Square extends React.Component {
 
+const Square = ({ onClick, value }) => {
 
-    render() {
     return (
       //state 상태 'X' 설정
-      <button className="square" onClick={ ()=> { this.setState({value: 'X'})}}> 
-         {this.props.value} 
+      <button className="square" 
+          onClick={() => { this.props.onClick() }}> 
+          {value} 
       </button>
     )
 
-  }
 }
+
+export default Square;
