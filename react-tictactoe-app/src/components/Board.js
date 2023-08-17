44c6c315e8f1f2c9e35bd2436c0b5dbbf51ed34d
eunseo <92281453+les0498@users.noreply.github.com>
2023-08-17 19:46:56 +0900

    react-tictactoe-app

--- react-tictactoe-app/src/components/Board.js
+++ react-tictactoe-app/src/components/Board.js
@@ -5,12 +5,42 @@ import "./Board.css";
 
 const Board = () => {
   
-  const [squares,setSqures] = useState(Array(9).fill(null)); //getter와 setter
+  const [squares,setSquares] = useState(Array(9).fill(null)); //getter와 setter
+  const [xIsNext, setXIsNext] = useState(true); 
 
+  const calculateWinner = (squares) => {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ]
+      for (let index = 0; index < lines.length; index++) {
+        const [a, b, c] = lines[index]; 
+        if(squares[a] && squares[a] === squares[b] && squares[c]) {
+          return squares[a]; 
+        }
+      }
+      return null;
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+  
   const handleClick = (i) => {
     const newSquares = squares.slice();
-    newSquares[i] = 'X';
-    setSqures(newSquares);
+    newSquares[i] = xIsNext ? 'X' : 'O'; 
+    setSquares(newSquares);
+    setXIsNext(prev => !prev)
   }
 
   const renderSquare = (i) => {
@@ -18,9 +48,11 @@ const Board = () => {
         onClick={() => handleClick(i)} />
   }
 
+  
+
     return (
       <div>
-        <div className='status'>Next Player: X, O</div>
+        <div className='status'>{status}</div>
         <div className='board-row'>
           {renderSquare(0)}
           {renderSquare(1)}
--- react-tictactoe-app/src/components/Square.js
+++ react-tictactoe-app/src/components/Square.js
@@ -1,17 +1,12 @@
 import React from "react"; 
 import "./Square.css";
 
-
 const Square = ({ onClick, value }) => {
-
-    return (
-      //state 상태 'X' 설정
-      <button className="square" 
-          onClick={() => { this.props.onClick() }}> 
-          {value} 
-      </button>
-    )
-
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
 }
 
 export default Square;
